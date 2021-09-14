8907289e680c870dab2cc75bef055cae2dcd9148
Khusela Mphokeli <innothetechgeek@gmail.com>
2021-09-14 16:04:30 +0200

    more changes

---
+++ admin/PostController.php
@@ -0,0 +1,19 @@
+<?php
+
+
+class PostController{
+
+    private $crud;
+
+    public function __construct(){
+
+        $this->crud = new Crud();
+
+    }
+
+    public function addPost(){
+
+    }
+
+
+}
